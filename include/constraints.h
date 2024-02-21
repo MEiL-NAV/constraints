21fb31eaff9a14d00dafd9d672e0b0a0d85a7b07
Wojciech Gajda <wggajda@gmail.com>
2024-02-21 13:51:38 +0100

    Update constraints function and its derivative

--- include/constraints.h
+++ include/constraints.h
@@ -1,5 +1,5 @@
 #pragma once
 #include <eigen3/Eigen/Dense>
 
-extern Eigen::Vector<float, 13> constraints(const Eigen::Vector<float, 13> &state);
-extern Eigen::Matrix<float, 13, 13> constraints_derivative(const Eigen::Vector<float, 13> &state);
+extern Eigen::VectorXf constraints(const Eigen::Vector<float, 13> &state);
+extern Eigen::MatrixXf constraints_derivative(const Eigen::Vector<float, 13> &state);
--- src/constraints.cpp
+++ src/constraints.cpp
@@ -1,11 +1,15 @@
 #include "constraints.h"
 
-Eigen::Vector<float, 13> constraints(const Eigen::Vector<float, 13> &state) {
-    // TODO: implement
-    return Eigen::Vector<float, 13>::Zero();
+Eigen::VectorXf constraints(const Eigen::Vector<float, 13> &state) {
+    Eigen::VectorXf FI = Eigen::VectorXf(1);
+    auto quaterion = state.segment<4>(6);
+    FI(0) = quaterion.squaredNorm() - 1.0f;
+    return FI;
 }
 
-Eigen::Matrix<float, 13, 13> constraints_derivative(const Eigen::Vector<float, 13> &state) {
-    // TODO: implement
-    return Eigen::Matrix<float, 13, 13>::Identity();
+Eigen::MatrixXf constraints_derivative(const Eigen::Vector<float, 13> &state) {
+    Eigen::MatrixXf FId = Eigen::MatrixXf::Zero(1,13);
+    auto quaterion = state.segment<4>(6);
+    FId.block<1,4>(0,6) = 2.0f * quaterion.transpose();
+    return FId;
 }
